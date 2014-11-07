7d0340455d15cbe0bda4411e330787957ff52893
Artem Lobachev <artemy.l2011@gmail.com>
2014-11-07 18:26:38 +0300

    Abstract sort class includes abstract method "sort" and general methods isSorted, less, swap, print.

---
+++ sorts/src/org/artemyl/AbstractSort.java
@@ -0,0 +1,38 @@
+package org.artemyl;
+
+/**
+ * Created by artemlobachev on 31.10.14.
+ */
+public abstract class AbstractSort {
+
+    public abstract void sort(Comparable array[]);
+
+    @SuppressWarnings("unchecked") // assuming a and b are the objects of the same classes
+    public boolean less(Comparable a, Comparable b){
+        return a.compareTo(b) < 0;
+    }
+
+    public boolean isSorted(Comparable array[]){
+        for (int i = 0; i < array.length - 1; i++){
+            if (!this.less(array[i], array[i+1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void swap(Comparable array[], int firstNumber, int secondNumber){
+        if (firstNumber == secondNumber) return;
+        Comparable tmp = array[firstNumber];
+        array[firstNumber] = array[secondNumber];
+        array[secondNumber] = tmp;
+    }
+
+    public void print(Comparable array[]){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
