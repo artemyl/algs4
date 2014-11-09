aadbcfb376776462a301660b79b426f69f205d94
Artem Lobachev <artemy.l2011@gmail.com>
2014-11-09 13:48:52 +0300

    Quick sort is ready.

---
+++ sorts/src/org/artemyl/QuickSort.java
@@ -0,0 +1,31 @@
+package org.artemyl;
+
+/**
+ * Created by artemlobachev on 09.11.14.
+ */
+public class QuickSort extends AbstractSort {
+
+    protected int partition(Comparable array[], int min, int max){
+        int i = min + 1, j = max;
+        Comparable referenceElement = array[min];
+        do {
+            while (this.less(array[i], referenceElement) && (i < max)) i++;
+            while (this.less(referenceElement, array[j]) && (j > min)) j--;
+            if (i < j) this.swap(array, i, j);
+        } while (i < j);
+        this.swap(array, min, j);
+        return j;
+    }
+
+    protected void recursiveSort(Comparable array[], int min, int max){
+        if (max <= min) return;
+        int mid = this.partition(array, min, max);
+        this.recursiveSort(array, min, mid - 1);
+        this.recursiveSort(array, mid + 1, max);
+    }
+
+    @Override
+    public void sort(Comparable[] array) {
+        this.recursiveSort(array, 0, array.length - 1);
+    }
+}
